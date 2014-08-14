8700c91f4124e91c6a983eb5bdeb58d808ae4a41
Joergen Ibsen <jibz-git@stdip.com>
2014-08-14 08:50:49 +0200

    Improve error messages
    Replace fileinput with a custom class that allows error messages to
    show filename and line number.

--- yamltotm.py
+++ yamltotm.py
@@ -15,26 +15,77 @@ Note: Uses plistlib interface introduced in Python 3.4.
 """
 
 import argparse
-import fileinput
+import collections
 import plistlib
 import re
 
 import yaml
 
 
-userdict = {}
+FileRange = collections.namedtuple('FileRange', 'lo hi name')
+
+
+class MultiFile:
+    """
+    Array of lines from multiple files.
+    """
+
+    def __init__(self, filenames=None, mode='r'):
+        self.lines = []
+        self._files = []
+        if filenames is not None:
+            self.add_files(filenames, mode)
+
+    def __iter__(self):
+        return iter(self.lines)
+
+    def add_files(self, filenames, mode='r'):
+        """
+        Add lines from files to end of MultiFile.
+
+        :param filenames: List of filenames
+        """
+        base = len(self.lines)
+        for name in filenames:
+            with open(name, mode) as f:
+                self.lines.extend(f.readlines())
+                self._files.append(FileRange(base, len(self.lines), name))
+                base = len(self.lines)
+
+    def lookup_line(self, n):
+        """
+        Look up filename and line number from line index.
+
+        :param n: Absolute index of line (starting at 0)
+        :rtype: (str, int) or None
+        :return: Filename and line number (starting at 1)
+        """
+        for f in self._files:
+            if n >= f.lo and n < f.hi:
+                return f.name, n - f.lo + 1
+        else:
+            return None
+
+
+_mf = MultiFile()
+
+_userdict = {}
 
 
 def dictrepl(matchobj):
     if matchobj.group(1) == '$':
         return '$'
     else:
-        return userdict[matchobj.group(1)]
+        return _userdict[matchobj.group(1)]
 
 
 def str_constructor(loader, data):
     s = loader.construct_scalar(data)
-    s = re.sub('\$(\$|\w+)', dictrepl, s)
+    try:
+        s = re.sub('\$(\$|\w+)', dictrepl, s)
+    except KeyError as e:
+        name, line = _mf.lookup_line(data.start_mark.line)
+        print('KeyError in "{}", line {}:'.format(name, line), e)
     return s
 
 
@@ -50,11 +101,21 @@ if __name__ == '__main__':
     # if user supplied a dictionary, read it and add our own constructor
     # for str objects that performs lookup
     if args.dict:
-        userdict = yaml.safe_load(args.dict)
+        _userdict = yaml.safe_load(args.dict)
         yaml.add_constructor(u'tag:yaml.org,2002:str', str_constructor,
                              Loader=yaml.SafeLoader)
 
-    # read YAML and write tmTheme plist
-    with fileinput.input(args.infile) as f:
-        theme = yaml.safe_load(''.join(f))
+    # read YAML input files into MultiFile
+    _mf.add_files(args.infile)
+
+    # load YAML and write tmTheme plist
+    try:
+        theme = yaml.safe_load(''.join(_mf.lines))
+    except yaml.YAMLError as e:
+        if hasattr(e, 'problem_mark'):
+            name, line = _mf.lookup_line(e.problem_mark.line)
+            print('YAML error in "{}", line {}:'.format(name, line), e)
+        else:
+            print('YAML error:', e)
+    else:
         plistlib.dump(theme, args.outfile)
